ac9e56c5131174198414f41a1703ca35ee1f7fd8
kirakernel <115137992+kirakernel@users.noreply.github.com>
2023-12-20 18:54:35 -0300

    Booleans, comparision operators and logical operators

---
+++ phase01/python/booleans_comparision_operators_and_logical_operators.py
@@ -0,0 +1,20 @@
+# Comparision Operators
+print(5 < 3) # False - Less Than
+
+print(5 > 3) # True - Greater Than
+
+print(3 <= 3) # True - Less Than or Equal To
+
+print(3 >= 5) # False - Greater Than or Equal To
+
+print(3 == 5) # False - Equal To
+
+print(3 != 5) # True - No Equal To
+
+
+# Logical Operators
+print(5 < 3 and 5 == 5) # False - and - Evaluates if all provided statements are True
+
+print(5 < 3 or 5 == 5) # True - or - Evaluates if at least one of many statements is True
+
+print(not 5 < 3) # True - not - Flips the Bool Value
